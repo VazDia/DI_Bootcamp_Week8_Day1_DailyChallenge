2456f73705b3115cf03cc27af57b20bce6502513
VazDia <vdiarrassouba1@gmail.com>
2023-02-18 11:53:48 +0000

    Update;

---
+++ all_queries.sql
@@ -0,0 +1,10 @@
+-- We used the Hollywood database from the course with 1 table called actors and some
+actors and their informations.
+
+-- 1. Count how many actors are in the table.
+
+SELECT COUNT(*) FROM actors;
+
+-- 2. Try to add a new actor with some blank fields. What do you think the outcome will be ?
+
+-- Answer: The outcome will be an syntaxe error because the fields have not to be empty and also because of the type date.
